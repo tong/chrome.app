0c427f98c33c7a410257670b2d547f7344740494
tong <tong@disktree.net>
2012-12-24 14:50:02 +0100

    added own, slightly different versions of chrome.ContextMenus

---
+++ chrome/ContextMenus.hx
@@ -0,0 +1,56 @@
+package chrome;
+
+typedef OnClickData = {
+	menuItemId : Int,
+	?parentMenuItemId : Int,
+	?mediaType : String,
+	?linkUrl : String,
+	?srcUrl : String,
+	pageUrl : String,
+	?frameUrl : String,
+	?selectionText : String,
+	editable : String
+}
+
+@:native("chrome.contextMenus") extern class ContextMenus {
+	
+	static function create( 
+		createProperties : {
+			?type : String,
+			?id : String,
+			?title : String,
+			?checked : Bool,
+			?contexts : Array<String>,
+			?onclick : Void->Void, //OnClickData->Tab->Void,
+			?parentId : Int,
+			?documentUrlPatterns : Array<String>,
+			?targetUrlPatterns : Array<String>,
+			?enabled : Bool
+		},
+		?cb : Void->Void
+	) : Int;
+	
+	static function remove(
+		menuItemId : Int,
+		?cb : Void->Void
+	) : Void;
+	
+	static function removeAll( ?cb : Void->Void ) : Void;
+	
+	static function update(
+		id : String,
+		updateProperties : {
+			?type : String,
+			?title : String,
+			?checked : Bool,
+			?contexts : String,
+			?onclick : Void->Void,
+			?parentId : Int,
+			?documentUrlPatterns : Array<String>,
+			?targetUrlPatterns : Array<String>,
+			?enabled : Bool
+		},
+		?cb : Void->Void
+	) : Void;
+	
+}
