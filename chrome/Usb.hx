87bdec418090795946c6b4a4e9c5f5fdf7a6da17
tong <tong@disktree.net>
2013-01-11 19:01:54 +0100

    added chrome.Usb

---
+++ chrome/Usb.hx
@@ -0,0 +1,65 @@
+package chrome;
+
+typedef FindDevicesOptions = {
+	var vendorId : Int;
+	var productId : Int;
+}
+
+typedef Device = {
+	var vendorId : Int;
+	var productId : Int;
+	var handle : Int;
+}
+
+/*
+@:fakeEnum(String) enum Recipient {
+	device;
+	interface;
+	endpoint;
+	other;
+}
+*/
+
+typedef ControlTransferInfo = {
+	index : Int,
+	direction : Int, //Direction,
+	requestType : String, //RequestType,
+	recipient : String, //Recipient,
+	request : Int,
+	value : Int,
+	?length : Int,
+	?data : ArrayBuffer
+}
+
+typedef TransferResultInfo = {
+	?resultCode : Int,
+	?data : ArrayBuffer
+}
+
+typedef GenericTransferInfo = {
+	direction : String,
+	endpoint : Int,
+	?data : ArrayBuffer,
+	?length : Int
+}
+
+typedef IsochronousTransferInfo = {
+	var packetLength : Int;
+	var transferInfo : GenericTransferInfo;
+	var packets : Int;
+}
+
+@:native("chrome.usb") extern class Usb {
+
+	static function findDevices( options : FindDevicesOptions, cb : Array<Device>->Void ) : Void;
+	static function closeDevice( device : Device, cb : Void->Void ) : Void;
+	static function claimInterface( device : Device, interfaceNumber : Int, cb : Void->Void ) : Void;
+	static function releaseInterface( device : Device, interfaceNumber : Int, cb : Void->Void ) : Void;
+	static function setInterfaceAlternateSetting( device : Device, interfaceNumber : Int, alternateSetting : Int, cb : Void->Void ) : Void;
+	static function controlTransfer( device : Device, transferInfo : ControlTransferInfo, cb : TransferResultInfo->Void ) : Void;
+	static function bulkTransfer( device : Device, transferInfo : GenericTransferInfo, ?cb : TransferResultInfo->Void ) : Void;
+	static function interruptTransfer( device : Device, transferInfo : GenericTransferInfo, ?cb : TransferResultInfo->Void ) : Void;
+	static function isochronousTransfer( device : Device, transferInfo : IsochronousTransferInfo, ?cb : TransferResultInfo->Void ) : Void;
+	
+	//static var onEvent(default,null) : Event<UsbEvent->Void>;
+}
