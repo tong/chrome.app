34a3035ba092a09357985751a18f27a7b66b2587
tong <tong@disktree.net>
2014-01-26 18:51:19 +0100

    updated Serial api

--- chrome/Serial.hx
+++ chrome/Serial.hx
@@ -2,38 +2,68 @@ package chrome;
 
 import js.html.ArrayBuffer;
 
-typedef OpenOptions = {
-	var bitrate : Int;
+private typedef ConnectOptions = {
+	@:optional var persistent : Bool;
+	@:optional var name : String;
+	@:optional var bufferSize : Int;
+	@:optional var bitrate : Int;
+	@:optional var dataBits : Int;
+	@:optional var parityBit : Int;
+	@:optional var stopBits : Int;
+	@:optional var ctsFlowControl : Bool;
+	@:optional var receiveTimeout : Int;
+	@:optional var sendTimeout : Int;
 }
 
-typedef OpenInfo = {
+private typedef ConnectionInfo = {
 	var connectionId : Int;
+	var paused : Bool;
+	var persistent : Bool;
+	var name : String;
+	var bufferSize : Int;
+	var receiveTimeout : Int;
+	var sendTimeout : Int;
+	@:optional var bitrate : Int;
+	@:optional var dataBits : Int;
+	@:optional var parityBit : Int;
+	@:optional var stopBits : Int;
+	@:optional var ctsFlowControl : Bool;
 }
 
-typedef ReadInfo = {
-	var bytesRead : Int;
-	var data : ArrayBuffer;
+@:enum(String)
+private enum Error {
+	disconnected;
+	pending;
+	timeout;
+	system_error;
 }
 
-typedef WriteInfo = {
-	var bytesWritten : Int;
+private typedef SendInfo = {
+	var bytesSent : Int;
+	var error : Error;
 }
 
-typedef ControlSignalOptions = {
-	@:optional var cts : Bool;
-	@:optional var dcd : Bool;
-	@:optional var dtr : Bool;
-	@:optional var rts : Bool;
+private typedef Signals = {
+	var dcd : Bool;
+	var cts : Bool;
+	var ri : Bool;
+	var dsr : Bool;
 }
 
 @:require(chrome_app)
-@:native("chrome.serial") extern class Serial {
-	static function getPorts( cb : Array<String>->Void ) : Void;
-	static function open( port : String, ?options : OpenOptions, cb : OpenInfo->Void ) : Void;
-	static function close( connectionId : Int, cb : Bool->Void ) : Void;
-	static function read( connectionId : Int, cb : ReadInfo->Void ) : Void;
-	static function write( connectionId : Int, data : ArrayBuffer, cb : WriteInfo->Void ) : Void;
-	static function flush( connectionId : Int, cb : Bool->Void ) : Void;
-	static function getControlSignals( connectionId : Int, cb : Bool->Void ) : Void;
-	static function setControlSignals( connectionId : Int, options : ControlSignalOptions, cb : Bool->Void ) : Void;
+@:native("chrome.serial")
+extern class Serial {
+	static function getDevices( f : Array<{path:String}>->Void ) : Void;
+	static function connect( path : String, ?options : ConnectOptions, f : ConnectionInfo->Void ) : Void;
+	static function update( connectionId : Int, options : ConnectOptions, f : Bool->Void ) : Void;
+	static function disconnect( connectionId : Int, f : Bool->Void ) : Void;
+	static function setPaused( connectionId : Int, paused : Bool, f : Void->Void ) : Void;
+	static function getInfo( connectionId : Int, f : ConnectionInfo->Void ) : Void;
+	static function getConnections( f : Array<ConnectionInfo>->Void ) : Void;
+	static function send( connectionId : Int, data : ArrayBuffer, f : Dynamic->Void ) : Void;
+	static function flush( connectionId : Int, f : Bool->Void ) : Void;
+	static function getControlSignals( connectionId : Int, f : Bool->Void ) : Void;
+	static function setControlSignals( connectionId : Int, signals : {?dtr:Bool,?rts:Bool}, f : Bool->Void ) : Void;
+	static var onReceive : Event<{connectionId:Int,data:ArrayBuffer}->Void>;
+	static var onReceiveError : Event<{connectionId:Int,error:Error}->Void>;
 }
