a1dd33ebdd2cf3fa889130eb32387426fd369563
tong <tong@disktree.net>
2014-03-08 02:32:06 +0100

    updated/fixed Serial api

--- chrome/Serial.hx
+++ chrome/Serial.hx
@@ -2,14 +2,30 @@ package chrome;
 
 import js.html.ArrayBuffer;
 
-typedef SerialConnectOptions = {
+@:enum abstract SerialDataBits(String) {
+	var seven = "seven";
+	var eight = "eight";
+}
+
+@:enum abstract SerialParityBit(String) {
+	var no = "no";
+	var odd = "odd";
+	var even = "even";
+}
+
+@:enum abstract SerialStopBits(String) {
+	var one = "one";
+	var two = "two";
+}
+
+typedef SerialConnectionOptions = {
 	@:optional var persistent : Bool;
 	@:optional var name : String;
 	@:optional var bufferSize : Int;
 	@:optional var bitrate : Int;
-	@:optional var dataBits : Int;
-	@:optional var parityBit : Int;
-	@:optional var stopBits : Int;
+	@:optional var dataBits : SerialDataBits;
+	@:optional var parityBit : SerialParityBit;
+	@:optional var stopBits : SerialStopBits;
 	@:optional var ctsFlowControl : Bool;
 	@:optional var receiveTimeout : Int;
 	@:optional var sendTimeout : Int;
@@ -24,46 +40,48 @@ typedef SerialConnectionInfo = {
 	var receiveTimeout : Int;
 	var sendTimeout : Int;
 	@:optional var bitrate : Int;
-	@:optional var dataBits : Int;
-	@:optional var parityBit : Int;
-	@:optional var stopBits : Int;
+	@:optional var dataBits : SerialDataBits;
+	@:optional var parityBit : SerialParityBit;
+	@:optional var stopBits : SerialStopBits;
 	@:optional var ctsFlowControl : Bool;
 }
 
-//@:enum(String)
-private enum Error {
-	disconnected;
-	pending;
-	timeout;
-	system_error;
+@:enum abstract SerialSendInfo(String) {
+	var disconnected = "disconnected";
+	var pending = "pending";
+	var timeout = "timeout";
+	var system_error = "system_error";
 }
 
-typedef SendInfo = {
-	var bytesSent : Int;
-	var error : Error;
-}
-
-typedef Signals = {
+typedef SerialSignals = {
 	var dcd : Bool;
 	var cts : Bool;
 	var ri : Bool;
 	var dsr : Bool;
 }
 
+@:enum abstract SerialError(String) {
+	var disconnected = "disconnected";
+	var timeout = "timeout";
+	var device_lost = "device_lost";
+	var system_error = "system_error";
+}
+
+@:require(chrome)
 @:require(chrome_app)
 @:native("chrome.serial")
 extern class Serial {
-	static function getDevices( f : Array<{path:String}>->Void ) : Void;
-	static function connect( path : String, ?options : SerialConnectOptions, f : SerialConnectionInfo->Void ) : Void;
-	static function update( connectionId : Int, options : SerialConnectOptions, f : Bool->Void ) : Void;
+	static function getDevices( f : Array<{path:String,?vendorId:Int,?productId:Int,?displayName:String}>->Void ) : Void;
+	static function connect( path : String, ?options : SerialConnectionOptions, f : SerialConnectionInfo->Void ) : Void;
+	static function update( connectionId : Int, options : SerialConnectionOptions, f : Bool->Void ) : Void;
 	static function disconnect( connectionId : Int, f : Bool->Void ) : Void;
 	static function setPaused( connectionId : Int, paused : Bool, f : Void->Void ) : Void;
 	static function getInfo( connectionId : Int, f : SerialConnectionInfo->Void ) : Void;
 	static function getConnections( f : Array<SerialConnectionInfo>->Void ) : Void;
-	static function send( connectionId : Int, data : ArrayBuffer, f : Dynamic->Void ) : Void;
+	static function send( connectionId : Int, data : ArrayBuffer, f : SerialSendInfo->Void ) : Void;
 	static function flush( connectionId : Int, f : Bool->Void ) : Void;
-	static function getControlSignals( connectionId : Int, f : Bool->Void ) : Void;
+	static function getControlSignals( connectionId : Int, f : SerialSignals->Void ) : Void;
 	static function setControlSignals( connectionId : Int, signals : {?dtr:Bool,?rts:Bool}, f : Bool->Void ) : Void;
 	static var onReceive : Event<{connectionId:Int,data:ArrayBuffer}->Void>;
-	static var onReceiveError : Event<{connectionId:Int,error:Error}->Void>;
+	static var onReceiveError : Event<{connectionId:Int,error:SerialError}->Void>;
 }
